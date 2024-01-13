00dd6d614fdbe36d78ede8b256197fa43e3e930b
Jerry Lee <sanghyun5958@gmail.com>
2024-01-13 14:30:25 +0800

    updated for Qrcode Scanner feature

--- src/QrcodeScanner.js
+++ src/QrcodeScanner.js
@@ -1,70 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, TouchableOpacity, TouchableNativeFeedback, ActivityIndicator, Platform, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 class QrcodeScanner extends Component {
+  onBarCodeRead = (e) => {
+    if (this.props.onScan) {
+      this.props.onScan(e);
+    }
+  };
+
   render() {
-    const {
-      onPress,
-      buttonStyle,
-      type,
-      title,
-      titleStyle,
-      loading,
-      disabled,
-      ...attributes
-    } = this.props;
-
-    const containerStyle = {
-      ...styles.button,
-      ...buttonStyle,
-      backgroundColor: type === 'solid' ? 'blue' : 'transparent',
-      opacity: disabled ? 0.5 : 1,
-    };
-
-    const TouchableComponent = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+    const { style, cameraProps } = this.props;
 
     return (
-      <View style={containerStyle}>
-        <TouchableComponent onPress={loading || disabled ? null : onPress} {...attributes}>
-          {loading ? (
-            <ActivityIndicator size="small" color="#ffffff" />
-          ) : (
-            <Text style={[styles.text, titleStyle]}>{title}</Text>
-          )}
-        </TouchableComponent>
+      <View style={[styles.container, style]}>
+        <RNCamera
+          style={StyleSheet.absoluteFill}
+          onBarCodeRead={this.onBarCodeRead}
+          {...cameraProps}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    padding: 10,
-    borderRadius: 5,
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  text: {
-    color: 'white',
-    textAlign: 'center',
   },
 });
 
 QrcodeScanner.propTypes = {
-  onPress: PropTypes.func,
-  buttonStyle: PropTypes.object,
-  title: PropTypes.string,
-  titleStyle: PropTypes.object,
-  loading: PropTypes.bool,
-  disabled: PropTypes.bool,
-  type: PropTypes.oneOf(['solid', 'clear']),
+  onScan: PropTypes.func,
+  style: PropTypes.object,
+  cameraProps: PropTypes.object,
 };
 
 QrcodeScanner.defaultProps = {
-  type: 'solid',
-  loading: false,
-  disabled: false,
+  style: {},
+  cameraProps: {},
 };
 
 export default QrcodeScanner;
